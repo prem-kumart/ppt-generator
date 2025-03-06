ff0c549292485f68508cd660e47d0090f6974841
premkumart <tprem1718@gmail.com>
2025-03-05 23:55:22 -0500

    add remove insert slides feature

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base:"/ppt-generator",
+  base:"/ppt-generator",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
